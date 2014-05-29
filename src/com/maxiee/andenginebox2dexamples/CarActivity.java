91af8dd5b95dff357b52b964a277c4b2eca637cd
Maxiee <maxieewong@gmail.com>
2014-05-30 03:03:32 +0800

    finish a simple car.

--- src/com/maxiee/andenginebox2dexamples/CarActivity.java
+++ src/com/maxiee/andenginebox2dexamples/CarActivity.java
@@ -1,5 +1,6 @@
 package com.maxiee.andenginebox2dexamples;
 
+import org.andengine.entity.primitive.Rectangle;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.sprite.Sprite;
 import org.andengine.extension.physics.box2d.PhysicsConnector;
@@ -8,17 +9,22 @@ import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
 import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
 import org.andengine.opengl.texture.region.TextureRegion;
 
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.joints.PrismaticJointDef;
 import com.badlogic.gdx.physics.box2d.joints.RevoluteJoint;
 import com.badlogic.gdx.physics.box2d.joints.RevoluteJointDef;
 
 public class CarActivity extends Box2DInitActivity {
-	private Body wheelBody;
-	private Sprite wheelFace;
+	private Body wheelFirstBody;
+	private Body wheelLastBody;
+	private Sprite wheelFirstFace;
+	private Sprite wheelLastFace;
 	private BitmapTextureAtlas carAtlas;
 	private TextureRegion wheelRegion;
-	private RevoluteJoint mWheelRev;
+	private RevoluteJoint mWheelFirstRev;
+	private RevoluteJoint mWheelLastRev;
 	
 	@Override
 	public void onCreateResources(
@@ -39,38 +45,81 @@ public class CarActivity extends Box2DInitActivity {
 			OnPopulateSceneCallback pOnPopulateSceneCallback) {
 		super.onPopulateScene(pScene, pOnPopulateSceneCallback);
 		
-		wheelFace = new Sprite(
-				(cameraWidth / 2), 
-				(cameraHeight / 2),
+		wheelFirstFace = new Sprite(
+				400, 240,
 				wheelRegion, super.getVertexBufferObjectManager());
-		mScene.attachChild(wheelFace);
-
-		
-		wheelBody = PhysicsFactory.createCircleBody(
+		mScene.attachChild(wheelFirstFace);
+		wheelFirstBody = PhysicsFactory.createCircleBody(
 				mPhysicsWorld, 
-				wheelFace, 
+				wheelFirstFace, 
 				BodyType.DynamicBody, 
 				ONLY_WALL_FIXTURE_DEF);
-		
-		Sprite axleFace = new Sprite(
-				(cameraWidth / 2), 
-				(cameraHeight / 2),
+		mPhysicsWorld.registerPhysicsConnector(new PhysicsConnector(wheelFirstFace, wheelFirstBody, true, true));
+		Sprite axleFirstFace = new Sprite(
+				400, 240,
 				wheelRegion, super.getVertexBufferObjectManager());
-		axleFace.setScale((float) 0.5);
-		axleFace.setVisible(false);
+		axleFirstFace.setScale((float) 0.5);
+		axleFirstFace.setVisible(false);
+		Body axleFirstBody = PhysicsFactory.createCircleBody(mPhysicsWorld,
+				axleFirstFace, BodyType.DynamicBody, NO_FIXTURE_DEF);
+		mPhysicsWorld.registerPhysicsConnector(new PhysicsConnector(axleFirstFace, axleFirstBody, true, true));
+		RevoluteJointDef wheelFirstRev = new RevoluteJointDef();
+		wheelFirstRev.initialize(axleFirstBody, wheelFirstBody, axleFirstBody.getWorldCenter());
+		wheelFirstRev.enableMotor = true;
+		wheelFirstRev.motorSpeed = 10;
+		wheelFirstRev.maxMotorTorque = 2000;
+		mWheelFirstRev = (RevoluteJoint)mPhysicsWorld.createJoint(wheelFirstRev);
 		
-		Body axleBody = PhysicsFactory.createCircleBody(mPhysicsWorld,
-				axleFace, BodyType.DynamicBody, NO_FIXTURE_DEF);
+		wheelLastFace = new Sprite(
+				700, 240,
+				wheelRegion, super.getVertexBufferObjectManager());
+		mScene.attachChild(wheelLastFace);
+		wheelLastBody = PhysicsFactory.createCircleBody(
+				mPhysicsWorld, 
+				wheelLastFace, 
+				BodyType.DynamicBody, 
+				ONLY_WALL_FIXTURE_DEF);
+		mPhysicsWorld.registerPhysicsConnector(new PhysicsConnector(wheelLastFace, wheelLastBody, true, true));
+		Sprite axleLastFace = new Sprite(
+				700, 240,
+				wheelRegion, super.getVertexBufferObjectManager());
+		axleLastFace.setScale((float) 0.5);
+		axleLastFace.setVisible(false);
+		Body axleLastBody = PhysicsFactory.createCircleBody(mPhysicsWorld,
+				axleLastFace, BodyType.DynamicBody, NO_FIXTURE_DEF);
+		mPhysicsWorld.registerPhysicsConnector(new PhysicsConnector(axleLastFace, axleLastBody, true, true));
+		RevoluteJointDef wheelLastRev = new RevoluteJointDef();
+		wheelLastRev.initialize(axleLastBody, wheelLastBody, axleLastBody.getWorldCenter());
+		wheelLastRev.enableMotor = true;
+		wheelLastRev.motorSpeed = 10;
+		wheelLastRev.maxMotorTorque = 2000;
+		mWheelLastRev = (RevoluteJoint)mPhysicsWorld.createJoint(wheelLastRev);
 		
-		mPhysicsWorld.registerPhysicsConnector(new PhysicsConnector(axleFace, axleBody, true, true));
-		mPhysicsWorld.registerPhysicsConnector(new PhysicsConnector(wheelFace, wheelBody, true, true));
+		Rectangle carFace = new Rectangle(550, 300, 400, 100, this.getEngine().getVertexBufferObjectManager());
+		carFace.setColor(0.5f, 0.5f, 0.5f);
+		mScene.attachChild(carFace);
+		Body carBody = PhysicsFactory.createBoxBody(mPhysicsWorld, carFace, BodyType.DynamicBody, ONLY_WALL_FIXTURE_DEF);
+		mPhysicsWorld.registerPhysicsConnector(new PhysicsConnector(carFace, carBody));
 		
-		RevoluteJointDef wheelRev = new RevoluteJointDef();
-		wheelRev.initialize(axleBody, wheelBody, axleBody.getWorldCenter());
-		wheelRev.enableMotor = true;
-		wheelRev.motorSpeed = 100;
-		wheelRev.maxMotorTorque = 2000;
-		mWheelRev = (RevoluteJoint)mPhysicsWorld.createJoint(wheelRev);
+		Float a1 = (float) (-(Math.cos(Math.PI / 3)));
+		Float a2 = (float) ((Math.sin(Math.PI / 3)));
+		PrismaticJointDef springsFirst = new PrismaticJointDef();
+		springsFirst.initialize(carBody, axleFirstBody, axleFirstBody.getWorldCenter(), new Vector2(a1,a2));
+		springsFirst.collideConnected = true;
+		springsFirst.enableLimit = true;
+		springsFirst.enableMotor = true;
+		springsFirst.lowerTranslation = 0.1f;
+		springsFirst.upperTranslation = 0.4f;
+		mPhysicsWorld.createJoint(springsFirst);
+		
+		PrismaticJointDef springsLast = new PrismaticJointDef();
+		springsLast.initialize(carBody, axleLastBody, axleLastBody.getWorldCenter(), new Vector2(-1*a1,a2));
+		springsLast.collideConnected = true;
+		springsLast.enableLimit = true;
+		springsLast.enableMotor = true;
+		springsLast.lowerTranslation = 0.1f;
+		springsLast.upperTranslation = 0.4f;
+		mPhysicsWorld.createJoint(springsLast);
 		
 		pOnPopulateSceneCallback.onPopulateSceneFinished();
 	}
